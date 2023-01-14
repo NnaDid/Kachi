e683c9982931d8ed62ac043d26de4a99fb6ae2e6
MaxEfuru <fredrickmaximum@gmail.com>
2023-01-14 15:19:32 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// console.log("Hello world");
+// console.log(window);
+
+// alert("Hello world");
+// confirm("Are you sure you want to exit thre page");
+// prompt("What is you name please ? ","");
+
+//  WORKING WIth VARIABLE
+
+var x;   // declaration
+x = 45;  // variable initialiation
+x = 89;
+var y = 67;
+
+console.log(y,x);
+
+let z = x + y;  // Adddition operation %
+
+console.log(z);
+
+const w = y%x;
+console.log(w);
+
+
+/*
+OPERATORS
++ : ADDITION 
+- : Subtraction 
+* : Multiplication
+/ : Division
+% : MOduluo Division
+= : Assigment Operator
+-= : subtraction Assignment
++= : Addition Assignment
+*= : Multiplication  Assignment
+/= : Division Assignment
+%= : Remainder Assignment
+
+//  COMPARISON OPERATORS
+1. == : Equal
+2. != : Not Equal
+3. === : Equal both in value and type
+4. > : Greater than
+5. < : Lesser than
+6. >= : Greater than or equal to
+7. <= : Lesser than or equal to
+
+*/
+
+
+
+
+
+
+
+
